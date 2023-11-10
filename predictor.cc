455b3ab24b24ed9a1109366af29daf98166abd47
pepsipotty <44318003+pepsipotty@users.noreply.github.com>
2023-11-10 12:11:52 -0500

    clean stuff out
    issue - short serv2

--- predictor.cc
+++ predictor.cc
@@ -1,15 +1,7 @@
 #include "predictor.h"
 #include <cmath>
-
-//sengwei: Idealized Piecewise Linear Branch Prediction
-#define GLOBAL_HISTORY_LENGTH 8//8 bits
-#define LOCAL_HISTORY_LENGTH 8 //8 bits
-#define HIST_LEN 15 //history length
-#define WEIGHT_WIDTH 8
-
-
 /////////////// STORAGE BUDGET JUSTIFICATION ////////////////
-// Total storage budget: 32KB + 32 bits
+// Total storage budget: 64KB + 32 bits
 
 // Total PHT counters for Global predictor: 2^15
 // Total PHT size for global predictor = 2^15 * 2 bits/counter = 2^16 bits = 8KB
@@ -31,7 +23,6 @@ PREDICTOR::PREDICTOR(void){
   // Weights: W
   MIN_VAL = (1 << WEIGHT_WIDTH- 1) * -1;
   MAX_VAL = (1 << WEIGHT_WIDTH - 1) * 1;
-
   theta = static_cast<UINT32>(floor(2.14 * (HIST_LEN + 1)) + 20.58);
 
   initWeights(); // Initialize W
@@ -43,13 +34,13 @@ PREDICTOR::PREDICTOR(void){
 /////////////////////////////////////////////////////////////
 
 bool   PREDICTOR::GetPrediction(UINT32 PC){
-    int k = PC % (1 << GLOBAL_HISTORY_LENGTH);
+    int k = PC % (1 << ADDR_LEN);
 
     output = W[k][0][0];
 
     for (UINT32 i = 1; i <= HIST_LEN; i++) {
 
-        int j = GA[i] % (1 << LOCAL_HISTORY_LENGTH);
+        int j = GA[i] % (1 << GA_LEN);
 
         if (GHR[i] == true) {
 
@@ -71,7 +62,7 @@ bool   PREDICTOR::GetPrediction(UINT32 PC){
 /////////////////////////////////////////////////////////////
 
 void  PREDICTOR::UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget){
-   UINT32 k = PC % (1 << GLOBAL_HISTORY_LENGTH); 
+   UINT32 k = PC % (1 << ADDR_LEN); 
 
    if (abs(output) < theta || predDir != resolveDir) {
 
@@ -92,13 +83,11 @@ void  PREDICTOR::UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT3
           }
 
       }
-
 /////////////////////////////////////////////////////////////
-
       for (UINT32 i = 0 ; i < HIST_LEN ; i++)
         {
 
-          UINT32 j = GA[i] % (1 << GLOBAL_HISTORY_LENGTH);
+          UINT32 j = GA[i] % (1 << ADDR_LEN);
 
             if (GHR[i] == resolveDir)
             { 
@@ -150,12 +139,8 @@ void PREDICTOR::TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget) {
 // My beloved helper functions to keep the code above clean
 
 void PREDICTOR::initWeights() {
-  // UINT16 n = (1 << GLOBAL_HISTORY_LENGTH);
-  // UINT16 m = (1 << LOCAL_HISTORY_LENGTH);
-  // UINT32 W[n][m][HIST_LEN + 1]; 
-
-  for (UINT32 i = 0; i < (1 << GLOBAL_HISTORY_LENGTH); i++) {
-      for (UINT32 j = 0; j < (1 << LOCAL_HISTORY_LENGTH); j++) {
+  for (UINT32 i = 0; i < (1 << ADDR_LEN); i++) {
+      for (UINT32 j = 0; j < (1 << GA_LEN); j++) {
           for (UINT32 k = 0; k < (HIST_LEN + 1); k++) {
 
               W[i][j][k] = 0;
--- predictor.h
+++ predictor.h
@@ -5,45 +5,23 @@
 #include "tracer.h"
 
 #define UINT16      unsigned short int
-#define GLOBAL_HISTORY_LENGTH 8//8 bits
-#define LOCAL_HISTORY_LENGTH 8 //8 bits
+#define ADDR_LEN 8//8 bits
+#define GA_LEN 8 //8 bits
 #define HIST_LEN 15 //history length
+#define WEIGHT_WIDTH 8
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 
 class PREDICTOR{
 
-  // The state is defined for Gshare, change for your design
-
  private:
-  UINT32  *pht;          // pattern history table
-  UINT32  historyLength; // history length
-  UINT32  numPhtEntries; // entries in pht
-
-
-  //add for local predictor
-  //local pattern history table
-  //UINT32 pht_local_bit_size;
-  UINT32 *pht_local;
-  UINT32 numPhtLocalEntries;
-  int W[1 << GLOBAL_HISTORY_LENGTH][1 << LOCAL_HISTORY_LENGTH][HIST_LEN + 1];
   UINT32 GHR[HIST_LEN]; // global history register
   intptr_t GA[HIST_LEN];
+  int W[1 << ADDR_LEN][1 << GA_LEN][HIST_LEN + 1];
   int MIN_VAL;
   int MAX_VAL;
   int theta;
-
-
-  //branch history table for local branch predictor
-  UINT32 bht_history_length;
-  UINT32 numBhtEntries;
-  UINT32 bht_bit_size;
-  UINT16 *bht;
-
-  //for tournament counter
-  UINT32 *predictorChooseCounter;
-  UINT32 numTournamentCounter;
 
 
  public:
@@ -53,23 +31,18 @@ class PREDICTOR{
   PREDICTOR(void);
   bool    GetPrediction(UINT32 PC);
 
-  //add for tournament predictor
+    //add for tournament predictor
   bool    GetLocalPrediction(UINT32 PC);
   bool    GetGlobalPrediction(UINT32 PC);
 
   void    UpdatePredictor(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget);
   void    TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget);
-  void    trainPredictor(UINT32 PC, bool prediction);
-  int     output;
 
-  // Helper functions
+  // Contestants can define their own functions below
+  int output;
   void initWeights();
   void initGlobalHistoryRegister();
   void initGlobalAddress();
-
-
-  // Contestants can define their own functions below
-
 };
 
 
